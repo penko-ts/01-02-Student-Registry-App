9b6c57988c11727bced354fd1c18c94ba503092d
Penko Tsochev <penko.aprilci@gmail.com>
2024-10-22 00:44:48 +0300

    Update title

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "DevOps Registry App",
+      msg: "The new DevOps Registry App",
       students: students
     };
     res.render('home', model);
